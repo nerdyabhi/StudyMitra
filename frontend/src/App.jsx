90dcf9fbf8c5c672e8c85ab8682d42bbc089ba5e
AgrawalAman02 <amanhzb2003@gmail.com>
2025-02-05 01:46:24 +0530

    shadcn setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='text-red-500'>Hello world abhi sharma!</h1>
+      
     </>
   )
 }
